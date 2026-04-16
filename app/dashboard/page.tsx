7601619867a5e3c5bf0347452eedc756aae19fb7
Arin <arinmaddheshiya853@gmail.com>
2026-04-16 16:03:46 +0530

    fix typescript null issue

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,24 +8,31 @@ import React from "react";
 const Page = async () => {
   const playgrounds = await getAllPlaygroundForUser();
   console.log("Playgrounds:", playgrounds);
+
   return (
     <div className="flex flex-col justify-start items-center min-h-screen mx-auto max-w-7xl px-4 py-10">
+      
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full">
         <AddNewButton />
         <AddRepo />
       </div>
 
       <div className="mt-10 flex flex-col justify-center items-center w-full">
+        
         {playgrounds && playgrounds.length === 0 ? (
           <EmptyState />
         ) : (
           <ProjectTable
-            projects={playgrounds || []}
+            projects={(playgrounds || []).map((p: any) => ({
+              ...p,
+              description: p.description || ""
+            }))}
             onDeleteProject={deleteProjectById}
             onUpdateProject={editProjectById}
             onDuplicateProject={duplicateProjectById}
           />
         )}
+
       </div>
     </div>
   );
